c8682031fd5088f363343822fbf47713dbd3c228
Nina Andrup Pedersen <202006703@post.au.dk>
2022-12-07 12:38:33 +0000

    Fix shift

--- src/compiler/x86/backend.ml
+++ src/compiler/x86/backend.ml
@@ -353,8 +353,15 @@ let compile_insn (ctxt : ctxt) ((opt_local_var, insn) : uid option * insn) :
             ; (Cqto, [])
             ; (op_x86, [right_reg])
             ; (Popq, [~%Rdx]) ]
-        | Subq | Shlq | Shrq | Sarq ->
+        | Subq ->
             [(op_x86, [right_reg; left_reg]); (Movq, [left_reg; right_reg])]
+        | Shlq | Shrq | Sarq ->
+          (match right with
+            | Const i ->
+              let num = Imm (Lit i) in
+              [(op_x86, [num ; left_reg]) ; (Movq, [left_reg; right_reg])]
+            | _ -> raise BackendFatal
+          )
         | _ -> [(op_x86, [left_reg; right_reg])]
       in
       match (opt_local_var, op_x86) with
